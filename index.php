adb472f9aaa3b594b24ee876fff8a94f2fd1640a
JesusAntonioGP <103076819+JesusAntonioGP@users.noreply.github.com>
2022-06-03 05:56:16 +0000

    adds smallest form handler

--- index.php
+++ index.php
@@ -5,10 +5,26 @@
   <body>
     <h1>PHP Tests</h1>
    
-    <p>hello from github</p>
-   <?php
-    echo '<p>This is change from replit!</p>';
-    ?>
-    <p>change from vs code</p>
+    <h1> world's smallest form handler</h1>
+
+
+    <?php
+if (isset($_POST["FirstName"])){//show data
+      echo $_POST["FirstName"];
+  
+}else{//show form
+  echo '
+      <form method="post" action=""> 
+    <p>First Name:</p>  <input type="text"     name="FirstName" />
+      <p><input type="submit"/></p>
+    </form>
+    
+      ';
+}
+
+?>
+    
+   
+    
   </body>
 </html>
